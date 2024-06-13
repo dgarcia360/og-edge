46b1f3107ca0d280f2c866c5d3ba99447b32de19
David Garcia <hi@davidgarcia.dev>
2024-06-13 09:54:02 +0100

    Update dynamic-image.tsx

--- netlify/edge-functions/dynamic-image.tsx
+++ netlify/edge-functions/dynamic-image.tsx
@@ -7,25 +7,24 @@ export default function handler(req: Request) {
   const title = searchParams.get("title");
 
   return new ImageResponse(
-      <div
-        style={{
-          fontSize: 40,
-          color: 'black',
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          padding: '50px 200px',
-          textAlign: 'center',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        {title}
-      </div>
-    ),
+    <div
+      style={{
+        fontSize: 40,
+        color: 'black',
+        background: 'white',
+        width: '100%',
+        height: '100%',
+        padding: '50px 200px',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {title}
+    </div>,
     {
       width: 1200,
       height: 630,
-    },
+    }
   );
 }
